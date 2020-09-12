39e578d4f08fdb918882199a10f47d48036c4e66
1386255 <hopperkremer@gmail.com>
2020-09-12 14:54:00 -0400

    Continued building chatbot

--- first_file.py
+++ first_file.py
@@ -6,8 +6,21 @@ print("please work")
 
 def some_fn():
     favColor = input("what's your fav color?")
-    print("Sure you like " + favColor + " best?")
+
+    print("Are you sure you like " + favColor + " best?")
+    age = input("How old are you?")
+    print("Huh, I rememember when I was " + age + " years old.")
+    # Let's figure out how to get current year with python
+    print("you were born around the year " +
+          str(2020-int(age)) + " weren't you?")
+    if int(age) > 70:
+        print("You are too old to drink")
+    elif int(age) > 21:
+        print("You can drink")
+    elif int(age) > 16:
+        print("You are too young")
+    else:
+        print("You are not a valid age")
 
 
 some_fn()
-# test
